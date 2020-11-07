878c523cb0621e5424e45bfda700b0226580407c
lusteiger <leonardoxy7@gmail.com>
2020-11-07 12:56:26 +0800

    [xinyu lu] implement PostMapping interface of the EventController

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -2,7 +2,10 @@ package com.thoughtworks.capability.gtb.vo;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +17,11 @@ public class EventVo {
 
   private String id;
   private String name;
+
+
   private EventType type;
+
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private Date time;
   @JsonUnwrapped
   private UserVo user;
